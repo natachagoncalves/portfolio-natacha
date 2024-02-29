2281c85eae60bd7f9709e8f6a785fa41410468bf
Natacha Gonçalves <natachagoncalvesjobs@gmail.com>
2024-02-29 14:07:52 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*======================= type animation ========================*/
+
+let typed = new Typed(".typing", {
+    strings: ["","Front-End Developer", "Desenvolvedora Front-End"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: false
+});
